d6f3f772087e7bf9a2151fff808f8b567af9b1e9
Angel Guzman <angelguzmaning@gmail.com>
2021-08-05 22:14:59 -0400

    Unsubscribe from Redux store

--- src/state/storeHooks.ts
+++ src/state/storeHooks.ts
@@ -4,8 +4,8 @@ import { State, store } from './store';
 export function useStoreWithInitializer<T>(getter: (state: State) => T, initializer: () => unknown) {
   const [state, setState] = useState(getter(store.getState()));
   useEffect(() => {
-    store.subscribe(() => setState(getter(store.getState())));
     initializer();
+    return store.subscribe(() => setState(getter(store.getState())));
   }, [null]);
   return state;
 }
