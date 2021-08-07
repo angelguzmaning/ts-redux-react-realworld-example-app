51dab845888e367f5906fadc00e50fe727ec9c63
Angel Guzman <angelguzmaning@gmail.com>
2021-08-07 13:46:18 -0400

    Link to register in Login Page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -21,7 +21,7 @@ export function Login() {
           <div className='col-md-6 offset-md-3 col-xs-12'>
             <h1 className='text-xs-center'>Sign in</h1>
             <p className='text-xs-center'>
-              <a href=''>Need an account?</a>
+              <a href='/#/register'>Need an account?</a>
             </p>
 
             {renderErrors(errors)}
